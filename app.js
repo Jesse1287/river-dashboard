0f223449a555624ec3f193d89387234af0a85145
Jesse1287 <1corinthians13a@gmail.com>
2026-06-26 16:20:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ const getWMO = (code) => {
 
 const getWindDir = (degrees) => `transform: rotate(${degrees}deg); display: inline-block;`;
 
-// Updated weather fetch with HRRR model for US accuracy
 async function fetchWeather(lat, lon) {
-   // Remove '&models=hrrr_conus' from the end of the URL string
 const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago`;
     try {
         const response = await fetch(url);
@@ -129,3 +127,4 @@ function initChecklist() {
         });
     });
 }
+
